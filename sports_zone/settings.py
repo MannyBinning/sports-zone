875adfbe61cab7034c2d7e3e8120a327aef4477f
MannyBinning <manib58@live.co.uk>
2021-06-19 11:12:50 +0000

    Deployment

--- sports_zone/settings.py
+++ sports_zone/settings.py
@@ -121,12 +121,11 @@ WSGI_APPLICATION = 'sports_zone.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+       'ENGINE': 'django.db.backends.sqlite3',
+       'NAME': BASE_DIR / 'db.sqlite3',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
