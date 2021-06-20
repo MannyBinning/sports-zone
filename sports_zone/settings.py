694702e1ffd858caa6a7f10b320f3dfb8ba7b04b
MannyBinning <manib58@live.co.uk>
2021-06-20 18:15:19 +0000

    updated settings.py

--- sports_zone/settings.py
+++ sports_zone/settings.py
@@ -206,3 +206,4 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FORM_EMAIL = 'sports_zone@example.com'
+
