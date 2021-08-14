b7811942315078aecb823ad51f87c2d7df4a4aa2
MannyBinning <manib58@live.co.uk>
2021-08-14 13:11:27 +0000

    Added Form to submit messages.

--- contact/views.py
+++ contact/views.py
@@ -1,7 +1,32 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.conf import settings
+
+from .forms import ContactForm
 
 
 def contact(request):
-    """ A view to return the contact page """
+    """ Contact Us form """
 
-    return render(request, 'contact/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            contact_message = {
+                'full_name': request.POST['full_name'],
+                'email': request.POST['email'],
+                'message': request.POST['message'],
+            }
+            form.save()
+            messages.success(request, 'Your message has been delivered. \
+                A member of staff will get back to you shortly. Thank you.')
+        else:
+            messages.error(request, 'Message failed.  Please submit \
+                a Valid form.')
+
+    form = ContactForm()
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
